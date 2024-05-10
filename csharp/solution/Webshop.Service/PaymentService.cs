881afc0ba240eefc76a0fd078ca5edbd916323f4
Tord Tonstad Andersen <tord.andersen@bouvet.no>
2024-05-10 15:19:12 +0200

    Add PaymentService solution with tests

---
+++ csharp/solution/Webshop.Service/PaymentService.cs
@@ -0,0 +1,151 @@
+﻿namespace Webshop.Service;
+
+public interface IPaymentService
+{
+    PaymentResult HandlePayment(string userId, decimal amountInNok);
+}
+
+public class PaymentService : IPaymentService
+{
+    private readonly IThirdPartyPaymentIntegration _thirdPartyPaymentIntegration;
+    private readonly IUserService _userService;
+    private readonly INotificationService _notificationService;
+    private readonly ILogger? _logger;
+
+    public PaymentService(IThirdPartyPaymentIntegration thirdPartyPaymentIntegration, IUserService userService, INotificationService notificationService, ILogger? logger)
+    {
+        _thirdPartyPaymentIntegration = thirdPartyPaymentIntegration;
+        _userService = userService;
+        _notificationService = notificationService;
+        _logger = logger;
+    }
+
+    public PaymentResult HandlePayment(string userId, decimal amountInNok)
+    {
+        var user = _userService.GetUser(userId);
+        if (user == null)
+        {
+            var msg = $"Attempted to handle payment for non-existing user {userId}";
+            _logger?.LogError(msg);
+            return new PaymentResult { Code = PaymentResultCode.UserNotFound, ErrorMessage = msg };
+        }
+
+        if (amountInNok < 0)
+        {
+            return new PaymentResult { Code = PaymentResultCode.InvalidAmount, ErrorMessage = "Amount cannot be negative" };
+        }
+
+        var paymentOk = _thirdPartyPaymentIntegration.HandlePaymentForUser(userId, amountInNok, "WebShop payment");
+        if (!paymentOk)
+        {
+            var msg = $"Payment failed for user {userId} with amount {amountInNok}";
+            _logger?.LogError(msg);
+            return new PaymentResult { Code = PaymentResultCode.PaymentIntegrationFailed, ErrorMessage = msg };
+        }
+
+        _notificationService.SendMail(user.Email, "Payment succeeded");
+
+        return new PaymentResult { Code = PaymentResultCode.Success };
+    }
+}
+
+public class PaymentResult
+{
+    public bool Success => Code == PaymentResultCode.Success;
+    public PaymentResultCode Code { get; set; }
+    public string? ErrorMessage { get; set; }
+}
+
+public enum PaymentResultCode
+{
+    Success,
+    UserNotFound,
+    InvalidAmount,
+    PaymentIntegrationFailed
+}
+
+public interface IThirdPartyPaymentIntegration
+{
+    bool HandlePaymentForUser(string userId, decimal amountInNok, string message);
+}
+
+public class ThirdPartyPaymentIntegration : IThirdPartyPaymentIntegration
+{
+    private readonly IThirdPartyPaymentSystemClient _client;
+    private readonly ILogger _logger;
+
+    public ThirdPartyPaymentIntegration(IThirdPartyPaymentSystemClient client, ILogger logger)
+    {
+        _client = client;
+        _logger = logger;
+    }
+
+    public bool HandlePaymentForUser(string userId, decimal amountInNok, string message)
+    {
+        try
+        {
+            _client.WaitForConnection();
+            return _client.HandlePayment(userId, amountInNok, message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Payment failed: {ex.Message}");
+            return false;
+        }
+    }
+}
+
+public interface IThirdPartyPaymentSystemClient
+{
+    bool HandlePayment(string userId, decimal amountInNok, string message);
+    void WaitForConnection();
+}
+
+public class ThirdPartyPaymentSystemClient : IThirdPartyPaymentSystemClient
+{
+    public void WaitForConnection()
+    {
+        // Simulate that we need to connect to a third party system which is not reachable from non-production environment
+        Thread.Sleep(TimeSpan.FromDays(365));
+    }
+
+    public bool HandlePayment(string userId, decimal amountInNok, string message)
+    {
+        // Simulate some work that may take some time
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+        return true;
+    }
+}
+
+public interface ILogger
+{
+    void LogError(string message);
+    void LogWarning(string message);
+    void LogInfo(string message);
+}
+
+public class Logger : ILogger
+{
+    public void LogError(string message)
+    {
+        Log("Error", message);
+    }
+
+    public void LogWarning(string message)
+    {
+        Log("Warning", message);
+    }
+
+    public void LogInfo(string message)
+    {
+        Log("Info", message);
+    }
+
+    private void Log(string level, string message)
+    {
+        using (var writer = File.AppendText("\\someserversomewhere\\log.txt"))
+        {
+            writer.WriteLine($"[{level}] [{DateTime.UtcNow.ToString("s")}] {message}");
+        }
+    }
+}
---
+++ csharp/solution/Webshop.UnitTests/PaymentServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using FakeItEasy;
+using NUnit.Framework;
+using Webshop.Service;
+
+namespace Webshop.UnitTests;
+
+[TestFixture]
+public class PaymentServiceTests
+{
+    // Problem 1: Cannot fake the third party payment service
+    // Problem 2: Cannot simulate the user exist or does not exist
+    // Problem 3: The logger will try to log to a file on a server somewhere
+    // Problem 4: No way to verify that email has been sent
+    // Problem 5: No way to verify the result
+
+    [Test]
+    public void HandlePayment_WhenNegativeAmount_ShouldFail()
+    {
+        var thirdPartyPaymentIntegration = A.Fake<IThirdPartyPaymentIntegration>();
+        var userService = A.Fake<IUserService>();
+        var notificationService = A.Fake<INotificationService>();
+
+        var paymentService = new PaymentService(thirdPartyPaymentIntegration, userService, notificationService, null);
+
+        var result = paymentService.HandlePayment("user1", -1);
+
+        Assert.That(result.Success, Is.False);
+        Assert.That(result.Code, Is.EqualTo(PaymentResultCode.InvalidAmount));
+    }
+
+    [Test]
+    public void HandlePayment_WhenUserDoesNotExist_ShouldFail()
+    {
+        var thirdPartyPaymentIntegration = A.Fake<IThirdPartyPaymentIntegration>();
+        var userService = A.Fake<IUserService>();
+        var notificationService = A.Fake<INotificationService>();
+
+        A.CallTo(() => userService.GetUser("user2")).Returns(null);
+
+        var paymentService = new PaymentService(thirdPartyPaymentIntegration, userService, notificationService, null);
+
+        var result = paymentService.HandlePayment("user2", 123);
+
+        Assert.That(result.Success, Is.False);
+        Assert.That(result.Code, Is.EqualTo(PaymentResultCode.UserNotFound));
+    }
+
+    [Test]
+    public void HandlePayment_WhenPaymentIsSuccessful_ShouldSendEmailToUser()
+    {
+        var thirdPartyPaymentIntegration = A.Fake<IThirdPartyPaymentIntegration>();
+        var userService = A.Fake<IUserService>();
+        var notificationService = A.Fake<INotificationService>();
+
+        A.CallTo(() => thirdPartyPaymentIntegration.HandlePaymentForUser(A<string>.Ignored, A<decimal>.Ignored, A<string>.Ignored)).Returns(true);
+
+        var paymentService = new PaymentService(thirdPartyPaymentIntegration, userService, notificationService, null);
+
+        var result = paymentService.HandlePayment("user1", 123);
+
+        Assert.That(result.Success, Is.True, result.ErrorMessage);
+        A.CallTo(() => notificationService.SendMail(A<string>.Ignored, A<string>.Ignored)).MustHaveHappenedOnceExactly();
+    }
+}
